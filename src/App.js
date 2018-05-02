c7d6f53691d3d6711f60b3bc4e12217a6ec400f7
Jaro <jarohabiger@googlemail.com>
2018-05-02 21:42:49 +0200

    build a more meaningfull prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import {goodThings, badThing} from './things';
+import {Things} from './ThingsRenderer';
 
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counts: []
+    }
+  }
+
   render() {
     return (
-      <div/>
+      <div>
+        <h1><b>57 Milliarden Euro</b> will die Bundesregierung Zukünftig für Rüstung ausgeben. Das sind:</h1>
+        <Things counts={this.state.counts} things={goodThings} />
+
+        <h2>... Oder Einmal den Rüstungsetat Deutschlands?!</h2>
+        <Things counts={[]} things={[badThing]} />
+      </div>
     );
   }
 }
---
+++ src/things.js
@@ -0,0 +1,17 @@
+import teacher from '../assets/things-icons/teacher.svg'
+import nurse from '../assets/things-icons/nurse.svg'
+import culture from '../assets/things-icons/culture.svg'
+import transport from '../assets/things-icons/transport.svg'
+
+import death from '../assets/things-icons/death.svg'
+
+
+export const goodThings = [
+  {icon: teacher, cost: 1, text: 'Lehrerinnen'},
+  {icon: nurse, text: 'Krankenpfleger'},
+  {icon: culture, text: 'Jugendzentren'},
+  {icon: culture, text: 'Kostenlose Essen für Schulkinder'},
+  {icon: transport, text: 'Kostenlose ÖPNV Jahrestickets'}
+]
+
+export const badThing = {icon: death};
